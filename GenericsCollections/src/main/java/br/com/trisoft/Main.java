62fd097bf6090a33cf55da0bb17a6c6272d9f4b1
root <luiz.trisoft@gmail.com>
2018-08-15 14:18:46 -0300

    Modificação de GenericCollections

--- GenericsCollections/src/main/java/br/com/trisoft/Main.java
+++ GenericsCollections/src/main/java/br/com/trisoft/Main.java
@@ -3,16 +3,22 @@ package br.com.trisoft;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * List de Strings
+ * 
+ * @author Luiz
+ *
+ */
 public class Main {
 
 	public static void main(String[] args) {
 		List<String> list = new ArrayList<>();
-		
+
 		list.add("abc");
 		list.add("def");
 		list.add("ghi");
-		
-		for(String s: list) {
+
+		for (String s : list) {
 			System.out.println(s);
 		}
 	}
